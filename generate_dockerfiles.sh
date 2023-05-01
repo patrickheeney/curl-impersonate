90b4aa9a1dc19b8600a34461d1d131d65a1dbb6b
Patrick Heeney <patrickheeney@gmail.com>
2023-04-30 19:52:32 -0700

    Fix symlink

--- generate_dockerfiles.sh
+++ generate_dockerfiles.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-cat <<EOF | mustache - Dockerfile.template > chrome/Dockerfile
+cat <<EOF | ./mustache Dockerfile.template > chrome/Dockerfile
 ---
 chrome: true
 debian: true
 ---
 EOF
-cat <<EOF | mustache - Dockerfile.template > chrome/Dockerfile.alpine
+cat <<EOF | ./mustache Dockerfile.template > chrome/Dockerfile.alpine
 ---
 chrome: true
 alpine: true
 ---
 EOF
 
-cat <<EOF | mustache - Dockerfile.template > firefox/Dockerfile
+cat <<EOF | ./mustache Dockerfile.template > firefox/Dockerfile
 ---
 firefox: true
 debian: true
 ---
 EOF
 
-cat <<EOF | mustache - Dockerfile.template > firefox/Dockerfile.alpine
+cat <<EOF | ./mustache Dockerfile.template > firefox/Dockerfile.alpine
 ---
 firefox: true
 alpine: true
